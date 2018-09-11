f68c1e8dedcd7286f60da5169a1d11145f3b14ae
Tom Medhurst <tom.medhurst@vigilantapps.com>
2018-09-12 00:15:02 +0100

    feat(billing): show relevant information in paypal notification endpoint placeholder

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,25 @@ const api = require('./api');
 // == API FUNCTIONS == //
 
 exports.paypal_events = functions.https.onRequest((request, response) => {
-  console.log('params', JSON.stringify(request.params));
-  console.log('query', JSON.stringify(request.query));
-  console.log('method', JSON.stringify(request.method));
-  response.status(200).send('');
+  /**
+   * TODO:
+   * https://api.sandbox.paypal.com/v1/notifications/verify-webhook-signature
+   * {
+   * auth_algo: request.header('paypal-auth-algo'),
+   * cert_url: request.header('paypal-cert-url'),
+   * transmission_id: request.header('paypal-transmission-id'),
+   * transmission_sig: request.header('paypal-transmission-sig'),
+   * transmission_time: request.header('paypal-transmission-time'),
+   * webhook_id: functions.config().paypal.webhook_id, // My Apps & Credentials page
+   * webhook_event: request.body,
+   * }
+   */
+  console.log('method', request.method);
+  console.log('eventType', request.body.event_type);
+  console.log('body', JSON.stringify(request.body, {}, 2));
+  console.log('headers', JSON.stringify(request.headers, {}, 2));
+  console.log('my_webhook_id', functions.config().paypal.webhook_id);
+  response.status(200).send({});
 });
 
 /**
